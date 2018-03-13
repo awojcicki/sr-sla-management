2362676bc969d2c0de8ec886060967a7596d5f99
Jaroslaw Kutyla <jaroslawkutyla@gmail.com>
2018-03-13 14:04:02 -0700

    Init jobs page

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const routes = require('./routes')
+const bodyParser = require('body-parser')
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
+    .use(bodyParser.json()) // support json encoded bodies
+    .use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
+    .use('/', routes)
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'pug')
-    .get('/', (req, res) => res.render('pages/index'))
-    .get('/login', (req, res) => res.render('pages/login'))
+    .use((req, res, next) => {
+        res.status(404).send("Sorry can't find that!")
+    })
+
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
