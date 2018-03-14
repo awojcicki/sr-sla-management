26df76c6bcec1a6a7cfa3a32d5d460527d8a82ef
Adam Wójcicki <a.wojcicki@smartrecruiters.com>
2018-03-13 23:26:26 -0700

    chrome extension

---
+++ chrome-extension/addScheduledJobUnpostingButtons.js
@@ -0,0 +1,86 @@
+// const DELAYED_UNPUBLISH_TPL = "<div>Unpublish job on: </div>" +
+//     "<input type='checkbox' id='delayedUnpublish' name='delayedUnpublish'/>" +
+//     "<input type='date' id='delayedUnpostingDate' name='delayedUnpostingDate'/>" +
+//     "";
+
+
+var DELAYED_UNPUBLISH_TPL = `
+   <div>Unpublish job on? </div>
+  <input type='checkbox' id='delayedUnpublish' name='delayedUnpublish'/>
+  <input type='date' id='delayedUnpostingDate' name='delayedUnpostingDate'/>
+`;
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.jobId) {
+        let unpublishCheckbox = document.getElementById("delayedUnpublish");
+        let delayedUnpostingDate = document.getElementById("delayedUnpostingDate");
+
+        var unpostingDate = null
+
+        if (delayedUnpostingDate.value) {
+            unpostingDate = new Date(delayedUnpostingDate.value + "T"+ new Date().getHours() + ":00:00")
+        }
+
+        chrome.storage.sync.get({
+                hashKey: ""
+            },
+            function(items) {
+                const key = items.hashKey;
+                var url = 'https://sr-sla-management.herokuapp.com/' + key +'/api/jobs/schedule';
+                var data = {
+                    jobId: request.jobId,
+                    unpost: unpublishCheckbox.checked,
+                    unpostingDate: unpostingDate
+                }
+                jQuery.ajax({
+                    type: "POST",
+                    url: url,
+                    data: JSON.stringify(data),
+                    dataType: "json",
+                    contentType: 'application/json'
+                });
+            });
+
+
+
+
+    } else if (request.wizardPublishTab) {
+        setTimeout(() => waitForPublishButton(), 500);
+    }
+
+});
+
+
+
+
+function waitForPublishButton() {
+    let publishButton = document.getElementsByTagName("publish-button");
+
+    if (publishButton.length == 0) {
+        setTimeout(() => waitForPublishButton(), 500);
+    } else {
+        addDelayedUnpublishInputs();
+    }
+}
+
+function addDelayedUnpublishInputs() {
+
+    if (jQuery('#delayedUnpublish').length == 0) {
+
+        jQuery('publish-button')
+            .parent('div.column')
+            .parent('.column')
+            .append(DELAYED_UNPUBLISH_TPL)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ chrome-extension/scheduleUnpost.js
@@ -0,0 +1,41 @@
+var publishing = false;
+var publishTabId = null;
+
+chrome.webRequest.onBeforeRequest.addListener((data) => {
+        chrome.tabs.query({active: true}, function(tabs) {
+            publishTabId = tabs[0].id
+        });
+    },
+    {urls: ["https://*.smartrecruiters.com/backoffice/web/posting-wizard/jobs*"]})
+
+
+chrome.webRequest.onCompleted.addListener((data) => {
+    console.log('hohohoho', data);
+    publishing = true;
+},
+    {urls: ["https://*.smartrecruiters.com/backoffice/web/posting-wizard/jobs*"]})
+
+chrome.webRequest.onBeforeRequest.addListener((data) => {
+    if (publishing) {
+        publishing = false;
+        var jobId = /.*\/backoffice\/web\/data\/jobs\/(.*)\/job-ads.*/.exec(data.url)[1]
+        chrome.tabs.sendMessage(publishTabId, {jobId: jobId}, function(response) {});
+    }
+    },
+    {urls: ["https://*.smartrecruiters.com/backoffice/web/data/jobs/*"]}
+);
+
+
+chrome.tabs.onUpdated.addListener(
+    function(tabId, changeInfo, tab) {
+        //TODO: only first tab?
+        console.log(tabId, changeInfo, tab);
+
+        if (tab.url.includes("/app/jobs/ad") && tab.url.includes("publish")) {
+            chrome.tabs.sendMessage(tabId, {wizardPublishTab: true}, function(response) {});
+
+        }
+    }
+);
+
+
