c46356ca0cee1ebf3fee1cf2010c3baeaa91a949
Adam Wójcicki <a.wojcicki@smartrecruiters.com>
2018-03-14 03:57:42 -0700

    try/catch for unposting (#24)
    ignore empty unposting

--- services/jobs/jobs.js
+++ services/jobs/jobs.js
@@ -51,17 +51,26 @@ module.exports = new function() {
   this.cancelUnposting = (user, jobId) => {
       console.log("cancelling unposting for", user.id, jobId);
       return new Promise((resolve, reject) => {
-          srApi.get(user, '/jobs/' + jobId).then((job => {
-              query = {
-                  text: 'DELETE FROM scheduled_unpostings WHERE job_id = $1',
-                  values: [jobId]
-              }
+          try {
+              srApi.get(user, '/jobs/' + jobId).then((job => {
+                  query = {
+                      text: 'DELETE FROM scheduled_unpostings WHERE job_id = $1',
+                      values: [jobId]
+                  }
 
-              pool.query(query).then((resp) => {
-                  resolve()
-              }).catch(e => {reject(e)})
+                  pool.query(query).then((resp) => {
+                      resolve()
+                  }).catch(e => {
+                      reject(e)
+                  })
 
-          })).catch(e => { reject(e) })
+              })).catch(e => {
+                  reject(e)
+              })
+          } catch (e) {
+              reject(e)
+          }
+
       });
 
   }
@@ -70,35 +79,48 @@ module.exports = new function() {
 
       return new Promise((resolve, reject) => {
           console.log("perofrming unposting for", user.id, jobId, date);
-          srApi.get(user, '/jobs/' + jobId).then((job => {
-              if (job.postingStatus != 'NOT_PUBLISHED') {
-                  pool.query('SELECT * FROM scheduled_unpostings WHERE job_id = $1', [jobId]).then(response => {
+          if (date == null) {
+              reject("null unposting")
+          }
+          try {
+              srApi.get(user, '/jobs/' + jobId).then((job => {
+                  if (job.postingStatus != 'NOT_PUBLISHED') {
+                      pool.query('SELECT * FROM scheduled_unpostings WHERE job_id = $1', [jobId]).then(response => {
 
-                      var query = {}
-                      if (response.rowCount == 0) {
-                          query = {
-                              text: 'INSERT INTO scheduled_unpostings(job_id, user_id, unposting_date) VALUES($1, $2, $3)',
-                              values: [jobId, user.id, date]
+                          var query = {}
+                          if (response.rowCount == 0) {
+                              query = {
+                                  text: 'INSERT INTO scheduled_unpostings(job_id, user_id, unposting_date) VALUES($1, $2, $3)',
+                                  values: [jobId, user.id, date]
+                              }
+                          } else {
+                              query = {
+                                  text: 'UPDATE scheduled_unpostings SET unposting_date = $3, user_id = $2 where job_id = $1',
+                                  values: [jobId, user.id, date]
+                              }
                           }
-                      } else {
-                          query = {
-                              text: 'UPDATE scheduled_unpostings SET unposting_date = $3, user_id = $2 where job_id = $1',
-                              values: [jobId, user.id, date]
-                          }
-                      }
-                      console.log('scheduling', query)
-                      pool.query(query).then((resp) => {
-                          resolve()
-                      }).catch(e => {reject(e)})
+                          console.log('scheduling', query)
+                          pool.query(query).then((resp) => {
+                              resolve()
+                          }).catch(e => {
+                              reject(e)
+                          })
 
 
-                  }).catch(e => {reject(e)})
+                      }).catch(e => {
+                          reject(e)
+                      })
 
-              } else {
-                  reject("job istn't published")
-              }
+                  } else {
+                      reject("job istn't published")
+                  }
 
-          })).catch(e => { reject(e) })
+              })).catch(e => {
+                  reject(e)
+              })
+          } catch (e) {
+              reject(e)
+          }
       });
   }
 };
